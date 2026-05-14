cb72deae4a57f53354a5a7a895cc0b33e0ecfa25
Rohit Kumar <rohit2004jangra@gmail.com>
2026-05-14 21:35:47 +0530

    Minimum element in rotated inn array

---
+++ 153.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> nums = {4,5,6,7,0,1,2};
+
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
+    }
+
+    cout << "Minimum element = " << nums[left];
+
+    return 0;
+}
