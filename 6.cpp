e42dd4354cbcaaedebae331d450f779ed20373a8
Rohit Kumar <rohit2004jangra@gmail.com>
2026-05-30 22:27:08 +0530

    Add solution for zigzag conversion problem

---
+++ 6.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string convert(string s, int numRows) {
+    if (numRows == 1 || numRows >= s.size())
+        return s;
+
+    vector<string> rows(numRows);
+
+    int currRow = 0;
+    bool down = true;
+
+    for (char c : s) {
+        rows[currRow] += c;
+
+        if (currRow == 0)
+            down = true;
+        else if (currRow == numRows - 1)
+            down = false;
+
+        currRow += down ? 1 : -1;
+    }
+
+    string ans;
+
+    for (string row : rows)
+        ans += row;
+
+    return ans;
+}
+
+int main() {
+    string s = "PAYPALISHIRING";
+    int numRows = 3;
+
+    cout << convert(s, numRows);
+
+    return 0;
+}
