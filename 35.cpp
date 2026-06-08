340adc687dd634f0b6047957f600804a925190bf
Rohit Kumar <rohit2004jangra@gmail.com>
2026-06-08 22:22:30 +0530

    Add solution for search insert position in a sorted array

---
+++ 35.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+}
+
+int main() {
+    vector<int> nums = {1,3,5,6};
+    int target = 5;
+
+    cout << searchInsert(nums, target);
+
+    return 0;
+}
