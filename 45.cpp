d6dacc9abd974eca7afaec5d41850d4a6ef5669e
Rohit Kumar <rohit2004jangra@gmail.com>
2026-06-11 21:15:03 +0530

    Add solution for jumping game

---
+++ 45.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int jump(vector<int>& nums) {
+    int jumps = 0;
+    int currentEnd = 0;
+    int farthest = 0;
+
+    for (int i = 0; i < nums.size() - 1; i++) {
+        farthest = max(farthest, i + nums[i]);
+
+        if (i == currentEnd) {
+            jumps++;
+            currentEnd = farthest;
+        }
+    }
+
+    return jumps;
+}
+
+int main() {
+    vector<int> nums = {2, 3, 1, 1, 4};
+
+    cout << "Minimum jumps = " << jump(nums);
+
+    return 0;
+}
